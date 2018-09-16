fa2877700bc06b8209cccd9fc45dd5412a388604
Erick Henriquez <henriquez93550@yahoo.com>
2018-09-15 19:33:49 -0500

    working copy

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+//function for the finish button
+function check() {
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var correct = 0;
+
+        if (question1 === "head") {
+            correct++;
+        }
+        if (question2 === "3 years") {
+            correct++;
+        }
+        if (question3 === "elephant") {
+            correct++;
+        }
+        if (question4 === "koala") {
+            correct++;
+        }
+        //if more are correct then it prints out a message depending on the order of score [0,1,2..etc]
+var messages = ["Great job!", "Thats is just okay", "You really need to do better"];
+
+//
+var range;
+        if (correct < 1) {
+            score = 2;
+        }
+        if (correct > 0 && correct < 3) {
+            score = 1; 
+        }
+        if (correct > 2) {
+            score = 0;
+        }
+
+        
+//writes information on to the HTML and shows it to user
+document.getElementById("after-submit").style.visibility = "visible";
+//types up message to display to user
+document.getElementById("message").innerHTML = messages[score];
+//tells user how many questions they got correct
+document.getElementById("number-correct").innerHTML = "You got " + correct + " correct.";           
+
+}
